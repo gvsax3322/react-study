4ed5bb970c266b8a3ae96e038ef4086e5b1564c9
gvsax3322 <cosbe3822@gmail.com>
2023-11-23 16:52:02 +0900

    docs : React Router 이해 1

---
+++ src/pages/Ceo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2>안녕</h2>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Map.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Map = () => {
+  return (
+    <div>
+      <h3>바이</h3>
+    </div>
+  );
+};
+
+export default Map;
