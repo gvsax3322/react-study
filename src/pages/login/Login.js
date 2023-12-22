18170d28a97ac3b85f7fe1ec4c0cdf143196991d
gvsax3322 <cosbe3822@gmail.com>
2023-12-22 11:50:40 +0900

    docs: 가짜로그인

---
+++ src/pages/login/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      Login
+      <div>
+        <button>로그인 값 받기</button>
+        <button>로그인 흉내</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
